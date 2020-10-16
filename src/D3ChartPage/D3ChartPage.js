4820feee1df2b5520d72445dccfc391106e2a16e
Priya Reddy <saipriyareddy94@gmail.com>
2020-10-16 14:56:57 -0400

    Added scss to reactapplication

--- src/D3ChartPage/D3ChartPage.js
+++ src/D3ChartPage/D3ChartPage.js
@@ -12,8 +12,17 @@ const D3ChartPage = props => {
     .arc()
     .innerRadius(110)
     .outerRadius(170);
-  const colors = d3.scaleOrdinal(d3.schemeCategory10);
-  const format = d3.format(".2f");
+  const colors = 
+  d3.scaleOrdinal(d3.scaleOrdinal)
+  .range([ 'rgba(255, 99, 132, 0.5)',
+  'rgba(54, 162, 0, 0.8)',
+  'rgba(0, 255, 230, 0.2)',
+  'rgba(22, 256, 192, 0.7)',
+  'rgba(153, 102, 255, 0.5)',
+  'rgba(0, 159, 64, 0.2)',
+  'rgba(33, 159, 64, 0.3)',
+  'rgba(55, 99, 255, 0.2)',
+  'rgba(244, 244, 0, 0.7)']);
 
   useEffect(
     () => {
@@ -46,7 +55,7 @@ const D3ChartPage = props => {
         .attr("text-anchor", "middle")
         .attr("alignment-baseline", "middle")
         .attr("transform", d => `translate(${createArc.centroid(d)})`)
-        .style("fill", "white")
+        .style("fill", "black")
         .style('text-anchor', 'middle')
         .style('font-size', 12)
         .text(d => d.data.title);
